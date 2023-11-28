3e9d137630f06c00bac6a49caa5af164e0b756ee
Anshuman Kumar <mahianshuman491@gmail.com>
2023-11-28 16:54:02 +0530

    searc by category on search Button Click implement

--- src/UI-Components/HeaderComponent.js
+++ src/UI-Components/HeaderComponent.js
@@ -1,15 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import Logo from '../Logo.png'
 import { useDispatch, useSelector } from 'react-redux'
 import { userLogOut } from '../Slice/UserSlice';
 import { ToastContainer, toast } from 'react-toastify';
-
+import axios from 'axios'
 
 function HeaderComponent() {
+  const [IsSearchVisible, setSearchVisible] = useState(false);
+  const [search, setSearch] = useState("")
+  const [searctProduct, setSearctProduct] = useState([])
   const { cartTotalQuantity } = useSelector((state) => state.cart);
   const { CurrentUser, isLoggedIn } = useSelector((state) => state.User);
   const dispatch = useDispatch();
+
+
+  const handleOnSearchInput = (e) => {
+    setSearch(e.target.value);
+  }
+
+  const handleSearchButtonClick = (e) => {
+    if (search === "") {
+      toast.error("Search Box Can't Be Empty", {
+        position: "bottom-center",
+        autoClose: 3000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+      });
+    } else {
+      setSearchVisible(true);
+      axios(`http://localhost:5000/search/${search}`).then((response) => {
+        setSearctProduct(response.data)
+      });
+    }
+
+  }
 
   const handleHamButtonClick = (e) => {
     const navBar = document.querySelector(".sideNavbar");
@@ -33,45 +62,45 @@ function HeaderComponent() {
   }
   return (
     <>
-    <header className='App--Header'>
-      <img src={Logo} alt="AppLogo" className='AppLogo' />
+      <header className='App--Header'>
+        <img src={Logo} alt="AppLogo" className='AppLogo' />
 
-      <div className="searchContainer">
-        <input type="text" name='SearctProduct' id='SearchProduct' placeholder='Search Here For Product' />
-        <button className='searchProductBtn'><i className="fa-solid fa-magnifying-glass searchProductBtnIcon"></i></button>
-      </div>
-
-      <div className="userContainer">
-        <div className="userBox">
-          <i className="fa-solid fa-user-tie userIcon"></i>
-          <h2 className='UserName'>{CurrentUser?.User[0] ? CurrentUser.User[0].userName.slice(0, 1) : "U"}</h2>
-          <span className='itemCountLabel'>{isLoggedIn ? cartTotalQuantity : 0}</span>
+        <div className="searchContainer">
+          <input type="text" name='SearctProduct' id='SearchProduct' placeholder='Search Product By Brand Name' onChange={handleOnSearchInput} />
+          <button className='searchProductBtn' onClick={handleSearchButtonClick}><i className="fa-solid fa-magnifying-glass searchProductBtnIcon"></i></button>
         </div>
-        <ul className="userDropDown">
+
+        <div className="userContainer">
+          <div className="userBox">
+            <i className="fa-solid fa-user-tie userIcon"></i>
+            <h2 className='UserName'>{CurrentUser?.User[0] ? CurrentUser.User[0].userName.slice(0, 1) : "U"}</h2>
+            <span className='itemCountLabel'>{isLoggedIn ? cartTotalQuantity : 0}</span>
+          </div>
+          <ul className="userDropDown">
 
 
-          {
-            isLoggedIn ?
-              <>
+            {
+              isLoggedIn ?
+                <>
 
-                <Link className="dropDownItem" to="/cart"><i className="fa-solid fa-cart-shopping dropDownnitemIcon"></i> <span className='itemLabel'>Cart</span> </Link>
+                  <Link className="dropDownItem" to="/cart"><i className="fa-solid fa-cart-shopping dropDownnitemIcon"></i> <span className='itemLabel'>Cart</span> </Link>
 
-                <li className="dropDownItem" onClick={handleLogOut} ><i className="fa-solid fa-right-from-bracket dropDownnitemIcon"></i> <span className='itemLabel'>Log Out</span> </li>
-              </>
-              :
-              <>
-                <Link className="dropDownItem" to="/user/register"><i className="fa-solid fa-user-plus dropDownnitemIcon"></i><span className='itemLabel'>Register</span></Link>
-                <Link className="dropDownItem" to="/user/login"><i className="fa-solid fa-right-to-bracket dropDownnitemIcon"></i><span className='itemLabel'>Log In</span></Link>
-              </>
-          }
-        </ul>
-      </div>
+                  <li className="dropDownItem" onClick={handleLogOut} ><i className="fa-solid fa-right-from-bracket dropDownnitemIcon"></i> <span className='itemLabel'>Log Out</span> </li>
+                </>
+                :
+                <>
+                  <Link className="dropDownItem" to="/user/register"><i className="fa-solid fa-user-plus dropDownnitemIcon"></i><span className='itemLabel'>Register</span></Link>
+                  <Link className="dropDownItem" to="/user/login"><i className="fa-solid fa-right-to-bracket dropDownnitemIcon"></i><span className='itemLabel'>Log In</span></Link>
+                </>
+            }
+          </ul>
+        </div>
 
 
-      <i className="fa-solid fa-bars hamNavButton" onClick={handleHamButtonClick}></i>
-    </header>
+        <i className="fa-solid fa-bars hamNavButton" onClick={handleHamButtonClick}></i>
+      </header>
 
-     <ToastContainer
+      <ToastContainer
         position="top-center"
         autoClose={3000}
         hideProgressBar={false}
@@ -83,6 +112,24 @@ function HeaderComponent() {
         pauseOnHover
         theme="light"
       />
+      {
+        IsSearchVisible && <div className="serachResultContainer">
+          {
+            searctProduct?.map((products) => {
+              return <div className='searchResultBox' key={products.id}>
+                <img src={products.images[0]} alt="productImage" className={`SearchProductImage`} />
+                <p className="SearchedProdcut--price">
+                  <span className="Dprice">₹{products.Dprice}</span>
+                  <span className="DiscountPercentage">{products.discountPercentage}%off</span>
+                </p>
+                <p className="searchedProductRating">{products.rating}<i className="fa-solid fa-star"></i></p>
+                <Link to={`/product/${products.title.slice(0, 18)}-${products.id}`} className='searchProductViewLink' onClick={()=>setSearchVisible(false)}>Details </Link>
+              </div>
+            })
+          }
+
+        </div>
+      }
     </>
   )
 }
